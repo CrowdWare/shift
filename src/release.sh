a34fd3c705802d87e1f71dc9dc71282ee3c9e478
Artanidos <artanidos@gmail.com>
2021-02-10 10:45:46 +0000

    upd

--- src/release.sh
+++ src/release.sh
@@ -9,4 +9,4 @@ make install INSTALL_ROOT=/Users/art/SourceCode/Shift/src/build-project/
 /Users/art/qt/5.12.3/android_armv7/bin/androiddeployqt --release --input /Users/art/SourceCode/Shift/src/build-project/android-libshift.so-deployment-settings.json --output /Users/art/SourceCode/Shift/src/build-project --android-platform android-28 --jdk /home/art/Software/jdk1.8.0_231/bin/java --gradle
 cd ..
 java -jar ../uber-apk-signer-1.2.1.jar --apks /Users/art/SourceCode/Shift/src/build-project/build/outputs/apk/release/build-project-release-unsigned.apk
-cp /Users/art/SourceCode/Shift/src/build-project/build/outputs/apk/release/build-project-release-aligned-debugSigned.apk /Users/art/SourceCode/Shift/web/content/shift-1.0.1-signed.apk
+mv /Users/art/SourceCode/Shift/src/build-project/build/outputs/apk/release/build-project-release-aligned-debugSigned.apk /Users/art/SourceCode/Shift/src/build-project/build/outputs/apk/release/shift-1.0.1-signed.apk
