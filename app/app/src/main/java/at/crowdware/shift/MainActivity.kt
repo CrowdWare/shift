45e94896a23083883d76a5d9a492c40dabff9974
Artanidos <artanidos@gmail.com>
2023-05-03 23:44:31 +0200

    Icon changed to transparent

--- app/app/src/main/java/at/crowdware/shift/MainActivity.kt
+++ app/app/src/main/java/at/crowdware/shift/MainActivity.kt
@@ -30,7 +30,6 @@ class MainActivity : AppCompatActivity() {
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
-        //setSupportActionBar(binding.appBarMain.toolbar)
 
         /*binding.appBarMain.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
--- app/app/src/main/java/at/crowdware/shift/RegistrationActivity.kt
+++ app/app/src/main/java/at/crowdware/shift/RegistrationActivity.kt
@@ -1,9 +1,14 @@
 package at.crowdware.shift
 
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 
 class RegistrationActivity : AppCompatActivity() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+        supportActionBar?.hide()
+    }
 }
