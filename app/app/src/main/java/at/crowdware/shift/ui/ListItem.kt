d6e60b7cad2ddc57ad39a4ac569368ab3261ec02
Artanidos <artanidos@gmail.com>
2023-05-04 13:07:12 +0200

    Upd

---
+++ app/app/src/main/java/at/crowdware/shift/ui/ListItem.kt
@@ -0,0 +1,7 @@
+package at.crowdware.shift.ui
+
+class ListItem {
+    var date: String? = null
+    var text: String? = null
+    var value: String? = null
+}
--- app/app/src/main/java/at/crowdware/shift/ui/home/HomeFragment.kt
+++ app/app/src/main/java/at/crowdware/shift/ui/home/HomeFragment.kt
@@ -1,12 +1,17 @@
 package at.crowdware.shift.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import at.crowdware.shift.R
 import at.crowdware.shift.databinding.FragmentHomeBinding
+
 
 class HomeFragment : Fragment() {
 
@@ -27,6 +32,11 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        var array: Array<Any> = arrayOf("Melbourne", "Vienna", "Vancouver", "Toronto", "Calgary", "Adelaide", "Perth", "Auckland", "Helsinki", "Hamburg", "Munich", "New York", "Sydney", "Paris", "Cape Town", "Barcelona", "London", "Bangkok")
+        val transactions: ListView = binding.transactions
+        val adapter = ArrayAdapter(requireContext(), R.layout.list_view_item, array)
+
+        transactions.setAdapter(adapter)
         return root
     }
 
