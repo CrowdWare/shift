bc46e1335259832f5407e350e03dfa11dbbc1de9
Artanidos <artanidos@gmail.com>
2023-05-07 20:15:12 +0200

    refactoring

--- app/app/src/main/java/at/crowdware/shift/logic/Transaction.kt
+++
@@ -1,7 +0,0 @@
-package at.crowdware.shift.logic
-
-import java.io.Serializable
-import java.util.Date
-
-data class Transaction(val amount: Long, val from: String, val date: Date, val description: String):
-    Serializable
--- app/app/src/main/java/at/crowdware/shift/ui/ListItem.kt
+++
@@ -1,7 +0,0 @@
-package at.crowdware.shift.ui
-
-data class ListItem (
-    var date: String,
-    var text: String,
-    var value: String
-)
