a2f544c4a4716e8909e915630570c4e379cdd554
Artanidos <artanidos@gmail.com>
2023-05-15 06:35:38 +0200

    cr added

--- android/app/src/main/java/at/crowdware/shift/ui/widgets/AutoSizeText.kt
+++ android/app/src/main/java/at/crowdware/shift/ui/widgets/AutoSizeText.kt
@@ -1,3 +1,22 @@
+/****************************************************************************
+ * Copyright (C) 2023 CrowdWare
+ *
+ * This file is part of SHIFT.
+ *
+ *  SHIFT is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  SHIFT is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with SHIFT.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************************/
 package at.crowdware.shift.ui.widgets
 
 import androidx.compose.material3.Text
