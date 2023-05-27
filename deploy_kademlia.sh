7cd8784f6f1b5424c0d4139226e1dbb56ae04347
Artanidos <artanidos@gmail.com>
2023-05-28 01:27:51 +0200

    upd

--- deploy_kademlia.sh
+++
@@ -1,4 +0,0 @@
-cp kademlia/libs/kademlia.jar /home/shift/kademlia
-cd /home/shift/kademlia
-./run.sh
-cd /home/shift/sourcecode/Shift
---
+++ deploy_tracker.sh
@@ -0,0 +1,4 @@
+cp -r tracker /home/shift/tracker
+cd /home/shift/tracker
+./tracker.sh
+cd /home/shift/sourcecode/Shift
