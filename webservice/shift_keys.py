c0a204e57a26083472279e9ad89aa1f9e5b123ee
Artanidos <japp.olaf@gmail.com>
2023-06-19 22:22:24 +0200

    webservice moved to private repo

--- webservice/shift_keys.py
+++
@@ -1,8 +0,0 @@
-# exchange values before start
-
-SHIFT_API_KEY = "<key>"
-SHIFT_DB_PWD = "<pwd>"
-SHIFT_DB_USER = "<dbuser>"
-SHIFT_DATABASE = "<db>"
-SHIFT_SECRET_KEY = "<secret key>"
-SHIFT_CLIENT_KEY = "<secret key>"
